78c8f81307a1bd0a47c97c9e2a2a6685727ab903
g13234952822 <3346703192@qq.com>
2020-04-22 18:21:56 +0800

    第二次上传

--- src/main/java/cn/jiyun/controller/StudentController.java
+++ src/main/java/cn/jiyun/controller/StudentController.java
@@ -3,6 +3,7 @@ package cn.jiyun.controller;
 public class StudentController {
     public String shuchu(){
         System.out.println("hello!word!");
+        System.out.println("good!");
         return "";
     }
 }
